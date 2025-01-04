42c7f04679b1c5ff72935cc28eec4a1a70b0b1b3
FerminDiaz98 <fermindiazpedrero@gmail.com>
2025-01-04 16:23:04 -0300

    fixed mobile compatibility

--- docs/main.js
+++ docs/main.js
@@ -1,24 +1,26 @@
-let newY = 0, startY = 0;
+let newY = startY = initialStartY = 0;
 const card = document.getElementById('card')
 
-card.addEventListener('mousedown', mouseDown)
+card.addEventListener('pointerdown', mouseDown)
 
 function mouseDown(e){
-    startY = e.clientY
-
-    document.addEventListener('mousemove', mouseMove)
-    document.addEventListener('mouseup', mouseUp)
+    initialStartY = startY = e.clientY
+    document.addEventListener('pointermove', mouseMove)
+    document.addEventListener('pointerup', mouseUp)
 }
-
 function mouseMove(e){
     newY = startY - e.clientY
     startY = e.clientY
-    //if (newY > 0){
-    //    newY = 0;
-    //}  
+    if (startY < initialStartY){
+        newY = 0;
+    }  
+    //console.log("NewY: "+newY+" startY: "+startY)
     card.style.top = (card.offsetTop - newY) + 'px'
 }
-
 function mouseUp(e){
-    document.removeEventListener('mousemove', mouseMove)
+    document.removeEventListener('pointermove', mouseMove)
+    while (startY > initialStartY){
+        startY = startY - 1
+        card.style.top = (card.offsetTop - 1) + 'px'
+    }
 }
